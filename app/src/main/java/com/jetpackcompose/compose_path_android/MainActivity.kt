2524e9ff07cdb7e46388ad38e2aef7eaf4f1de19
osawant023 <osawant023@gmail.com>
2025-04-18 18:04:15 +0530

    - changes -

--- app/src/main/java/com/jetpackcompose/compose_path_android/MainActivity.kt
+++ app/src/main/java/com/jetpackcompose/compose_path_android/MainActivity.kt
@@ -1,73 +1,66 @@
 package com.jetpackcompose.compose_path_android
 
-import android.app.Activity
-import android.app.Activity.RESULT_OK
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.IntentSenderRequest
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
-import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
-import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_PDF
-import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_FULL
-import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
-import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
-import com.jetpackcompose.compose_path_android.screens.NotesApp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.jetpackcompose.compose_path_android.core.navigation.Route
+import com.jetpackcompose.compose_path_android.presentation.document_scanner.DocumentScannerApp
+import com.jetpackcompose.compose_path_android.presentation.notes_list.notes_list.notes_list.NotesApp
+import com.jetpackcompose.compose_path_android.presentation.scene_animation.SceneAnimation
+import com.jetpackcompose.compose_path_android.presentation.start.ComposePathListScreen
 import com.jetpackcompose.compose_path_android.ui.theme.ComposePathTheme
-import kotlinx.coroutines.launch
-import java.io.File
-import java.io.FileOutputStream
+import com.jetpackcompose.compose_path_android.util.Something
 
+val LocalNavController = compositionLocalOf<NavHostController> {
+    error("No NavController found!")
+}
 class MainActivity : ComponentActivity() {
-    val localActivity = staticCompositionLocalOf<ComponentActivity> { this }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            Something()
             ComposePathTheme(dynamicColor = false) {
-                NotesApp()
+                val navController = rememberNavController()
+                CompositionLocalProvider(
+                    LocalNavController provides navController
+                ) {
+                    Scaffold { padding ->
+                        NavHost(
+                            modifier = Modifier.padding(padding),
+                            navController = navController,
+                            startDestination = Route.ComposePathListScreen,
+                        ) {
+                            composable<Route.ComposePathListScreen> {
+                                ComposePathListScreen()
+                            }
+
+                            composable<Route.NotesList> {
+                                NotesApp()
+                            }
+
+                            composable<Route.DocumentScanner> {
+                                DocumentScannerApp()
+                            }
+
+                            composable<Route.SharedElement> {
+                                SceneAnimation()
+                            }
+                        }
+                    }
+                }
             }
         }
     }
